fba099ca98224f5dfba62920d5c4b1b5054381cd
Omega <91365851+ojs201@users.noreply.github.com>
2024-09-05 22:07:32 +0900

    [level 2] Title: 올바른 괄호, Time: 10.84 ms, Memory: 10.9 MB -BaekjoonHub

--- 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.py
@@ -1,13 +1,17 @@
 def solution(s):
+    s_list = list(s)
     cnt = 0
-    for ch in s:
-        if ch == '(':
+
+    for i in range(len(s_list)):
+        if s_list[i] == '(':
             cnt += 1
         else:
             cnt -= 1
-        if cnt < 0:
-            return False
-        
-    return cnt == 0
-    
-    
+            if cnt < 0:
+                return False
+                break
+
+    if cnt == 0:
+        return True
+    else:
+        return False
